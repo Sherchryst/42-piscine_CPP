fe030ede3cbb6b1c090d7f59ef8c4b9227cab88b
sgah <sgah@42.student.fr>
2021-05-13 15:48:58 +0200

    try_this

--- CPP_Module02/ex01/Fixed.cpp
+++ CPP_Module02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: sgah <sgah@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 22:31:23 by sgah              #+#    #+#             */
-/*   Updated: 2021/05/12 23:36:13 by sgah             ###   ########.fr       */
+/*   Updated: 2021/05/13 15:48:38 by sgah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,4 +48,76 @@ Fixed &	Fixed::operator=(Fixed const & rhs)
 	if (this != &rhs)
 		this->_fix = rhs.getRawBits();
 	return *this;
+}
+
+Try
+
+#include "Fixed.hpp"
+#include <iostream>
+#include <cmath>
+
+const int Fixed::_fractionalBits = 8;
+
+Fixed::Fixed(void) : _value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(int const value)
+{
+	std::cout << "Int constructor called" << std::endl;
+
+	this->_value = value << Fixed::_fractionalBits;
+}
+
+Fixed::Fixed(float const value)
+{
+	std::cout << "Float constructor called" << std::endl;
+
+	this->_value = (int)roundf(value * (1 << Fixed::_fractionalBits));
+}
+
+Fixed::Fixed(Fixed const &src) : _value(src.getRawBits())
+{
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed & Fixed::operator=(Fixed const &rhs)
+{
+	std::cout << "Assignation operator called" << std::endl;
+
+	this->_value = rhs.getRawBits();
+
+	return *this;
+}
+
+int Fixed::getRawBits(void) const
+{
+	return this->_value;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->_value = raw;
+}
+
+int Fixed::toInt(void) const
+{
+	return this->_value >> Fixed::_fractionalBits;
+}
+
+float Fixed::toFloat(void) const
+{
+	return this->_value / (float)(1 << Fixed::_fractionalBits);
+}
+
+std::ostream &operator<<(std::ostream &o, Fixed const &rhs)
+{
+	o << rhs.toFloat();
+	return o;
 }

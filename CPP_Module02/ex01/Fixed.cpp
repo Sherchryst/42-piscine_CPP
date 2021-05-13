ac942c9cfb2dc6c900f63f01072a8fc477186948
Sherchryst GAH <sgah@e1r11p12.42.fr>
2021-05-13 16:14:46 +0200

    finish

--- CPP_Module02/ex01/Fixed.cpp
+++ CPP_Module02/ex01/Fixed.cpp
@@ -6,57 +6,15 @@
 /*   By: sgah <sgah@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 22:31:23 by sgah              #+#    #+#             */
-/*   Updated: 2021/05/13 15:48:38 by sgah             ###   ########.fr       */
+/*   Updated: 2021/05/13 16:13:54 by sgah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "Fixed.hpp"
-
-int const Fixed::_bits = 8;
-
-Fixed::Fixed(void): _fix(0)
-{
-	std::cout << "Default constructor called" << std::endl;
-}
-
-Fixed::Fixed(Fixed const & src)
-{
-	std::cout << "Copy constructor called" << std::endl;
-	*this = src;
-}
-
-Fixed::~Fixed(void)
-{
-	std::cout << "Destructor called" << std::endl;
-}
-
-int	Fixed::getRawBits(void) const
-{
-	std::cout << "getRawBits member function called" << std::endl;
-	return this->_fix;
-}
-
-void	Fixed::setRawBits(int const raw)
-{
-	std::cout << "setRawBits member function called" << std::endl;
-	this->_fix = raw;
-}
-
-Fixed &	Fixed::operator=(Fixed const & rhs)
-{
-	std::cout << "Assignation operator called" << std::endl;
-	if (this != &rhs)
-		this->_fix = rhs.getRawBits();
-	return *this;
-}
-
-Try
 
 #include "Fixed.hpp"
 #include <iostream>
 #include <cmath>
 
-const int Fixed::_fractionalBits = 8;
+const int Fixed::_bits = 8;
 
 Fixed::Fixed(void) : _value(0)
 {
@@ -67,19 +25,20 @@ Fixed::Fixed(int const value)
 {
 	std::cout << "Int constructor called" << std::endl;
 
-	this->_value = value << Fixed::_fractionalBits;
+	this->_value = value << Fixed::_bits;
 }
 
 Fixed::Fixed(float const value)
 {
 	std::cout << "Float constructor called" << std::endl;
 
-	this->_value = (int)roundf(value * (1 << Fixed::_fractionalBits));
+	this->_value = (int)roundf(value * (1 << Fixed::_bits));
 }
 
-Fixed::Fixed(Fixed const &src) : _value(src.getRawBits())
+Fixed::Fixed(Fixed const &src)
 {
 	std::cout << "Copy constructor called" << std::endl;
+	this->_value = src.getRawBits();
 }
 
 Fixed::~Fixed(void)
@@ -108,15 +67,15 @@ void Fixed::setRawBits(int const raw)
 
 int Fixed::toInt(void) const
 {
-	return this->_value >> Fixed::_fractionalBits;
+	return this->_value >> Fixed::_bits;
 }
 
 float Fixed::toFloat(void) const
 {
-	return this->_value / (float)(1 << Fixed::_fractionalBits);
+	return this->_value / (float)(1 << Fixed::_bits);
 }
 
-std::ostream &operator<<(std::ostream &o, Fixed const &rhs)
+std::ostream &	operator<<(std::ostream &o, Fixed const &rhs)
 {
 	o << rhs.toFloat();
 	return o;
--- CPP_Module02/ex01/Fixed.hpp
+++ CPP_Module02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: sgah <sgah@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 22:31:23 by sgah              #+#    #+#             */
-/*   Updated: 2021/05/12 23:35:23 by sgah             ###   ########.fr       */
+/*   Updated: 2021/05/13 16:07:26 by sgah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,27 @@ class Fixed {
 	public:
 
 		Fixed(void);
-
+		Fixed(int const value);
+		Fixed(float const value);
 		Fixed(Fixed const & src);
 		~Fixed(void);
 
 		int	getRawBits(void) const;
 		void	setRawBits(int const raw);
+		int		toInt(void) const;
+		float	toFloat(void) const;
 
 		Fixed &	operator=(Fixed const & rhs);
 
 	private:
 
-		int	_fix;
+		int					_value;
 
 		static int const	_bits;
 
 };
+
+std::ostream &	operator<<(std::ostream & o, Fixed const & rhs);
 
 
 #endif

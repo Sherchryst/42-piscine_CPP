09a4d5f3ab6bd6ed764ad8bd2748cca698c27cc3
Sherchryst GAH <sgah@e2r5p14.42.fr>
2021-05-18 15:39:09 +0200

    start

---
+++ CPP_Module03/ex00/FragTrap.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgah <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/18 15:29:32 by sgah              #+#    #+#             */
+/*   Updated: 2021/05/18 15:29:32 by sgah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_H
+# define FRAGTRAP_H
+
+# include <iostream>
+
+class FragTrap {
+
+	public:
+
+		FragTrap(void);
+
+		FragTrap(FragTrap const & src);
+		~FragTrap(void);
+
+
+	private:
+
+};
+
+
+#endif

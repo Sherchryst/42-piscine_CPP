d1e420e4cad88909c0dc94d1d44334a2dfeeaf21
Sherchryst GAH <sgah@e2r5p14.42.fr>
2021-05-18 16:10:43 +0200

    continue

--- CPP_Module03/ex00/FragTrap.hpp
+++ CPP_Module03/ex00/FragTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: sgah <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 15:29:32 by sgah              #+#    #+#             */
-/*   Updated: 2021/05/18 15:29:32 by sgah             ###   ########.fr       */
+/*   Updated: 2021/05/18 16:08:45 by sgah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,19 @@ class FragTrap {
 
 		FragTrap(FragTrap const & src);
 		~FragTrap(void);
-
+		
+		void	Ranged
 
 	private:
+		int	_hitPoints;
+		int	_maxHitPoints;
+		int	_energyPoints;
+		int	_maxEnergyPoints;
+		int	_Level;
+		std::string	_name;
+		int	_meleeAttackDamage;
+		int	_rangedAttackDamage;
+		int	_amorDamageReduction;
 
 };
 

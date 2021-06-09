ecd9576736eb8cd32abd954745ef5be445ef9913
sgah <sgah@42.student.fr>
2021-06-09 22:31:30 +0200

    mathias_a_dit_de_la_merde

--- CPP_Module06/ex01/main.cpp
+++ CPP_Module06/ex01/main.cpp
@@ -6,16 +6,13 @@
 /*   By: sgah <sgah@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 14:48:13 by sgah              #+#    #+#             */
-/*   Updated: 2021/06/09 19:37:41 by sgah             ###   ########.fr       */
+/*   Updated: 2021/06/09 22:29:34 by sgah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cstdlib>
-#include <climits>
-#include <time.h>
 #include <iostream>
-#include <string>
-#include <cctype>
+#include <ctime>
 
 struct Data
 {
@@ -24,41 +21,48 @@ struct Data
 	std::string	s2;
 };
 
-void*		serialize(void)
+void	*serialize(void)
 {
-	std::srand (time(NULL));
-	Data*		res = new Data;
-	char		buf[8];
-	std::string	str;
+	std::srand(std::time(NULL));
 
-	for(size_t i = 0; i < 8; i++)
-		buf[i] = (std::rand() % 2) ? std::rand() % 10 + 48 :
-					(std::rand() % 2) ? std::rand() % 26 + 65 : std::rand() % 26 + 97;
-	res->s1 = std::string(buf, 8);
+	static const char	alnum[] =	"abcdefghijklmnopqrstuvwxyz"\
+									"ABCDEFGHIJKLMNOPQRSTUVWXYZ"\
+									"0123456789";
 
-	res->n = std::rand();
+	char		*res = new char[16 + sizeof(int)];
 
-	for(size_t i = 0; i < 8; i++)
-		buf[i] = (std::rand() % 2) ? std::rand() % 10 + 48 :
-									(std::rand() % 2) ? std::rand() % 26 + 65 : std::rand() % 26 + 97;
-	res->s2 = std::string(buf, 8);
+	for (int i = 0; i < 8; i++)
+		res[i] = alnum[std::rand() % 62];
 
-	return (reinterpret_cast<void*>(res));
+	*reinterpret_cast<int*>(res + 8) = std::rand();
+
+	for (int i = 8 + sizeof(int); (unsigned int)i < 16 + sizeof(int); i++)
+		res[i] = alnum[std::rand() % 62];
+	return (res);
 }
 
-Data*		deserialize(void* raw)
+Data 	*deserialize(void *serial)
 {
-	return (reinterpret_cast<Data*>(raw));
+	Data	*data = new Data;
+	char	*raw = reinterpret_cast<char*>(serial);
+
+	data->s1 = std::string(raw, 8);
+	data->s2 = std::string(raw + 8 + sizeof(int), 8);
+	data->n = *reinterpret_cast<int *>(raw + 8);
+	return (data);
 }
 
-int			main(void)
+int		main(void)
 {
-	Data*	data = deserialize(serialize());
+	void	*serial = serialize();
+	Data	*data = deserialize(serial);
 
-	std::cout<<"String 1: "<< data->s1 <<std::endl;
-	std::cout<<"int     : "<< std::dec << data->n <<std::endl;
-	std::cout<<"String 2: "<< data->s2 <<std::endl;
+	std::srand(time(0));
+	std::cout << "s1: " << data->s1 << std::endl;
+	std::cout << "s2: " << data->s2 << std::endl;
+	std::cout << "n : " << data->n << std::endl;
 
+	delete []reinterpret_cast<char*>(serial);
 	delete data;
 	return (0);
 }

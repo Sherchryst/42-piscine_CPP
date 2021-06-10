b444d324e3f6f15f9a08047909086595a64779e9
Sherchryst GAH <sgah@e2r4p22.42.fr>
2021-06-10 16:13:36 +0200

    correct

--- CPP_Module07/ex00/whatever.hpp
+++ CPP_Module07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: sgah <sgah@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/10 16:00:50 by sgah              #+#    #+#             */
-/*   Updated: 2021/06/10 16:06:13 by sgah             ###   ########.fr       */
+/*   Updated: 2021/06/10 16:13:13 by sgah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,23 @@
 # define WHATEVER_H
 
 template<typename T>
-void	swap(T a, T b)
+void		swap(T &a, T &b)
 {
-	T	tmp = a;
+	T	tmp;
 
+	tmp = a;
 	a = b;
 	b = tmp;
 }
 
 template<typename T>
-T		min(T a, T b)
+T const &	min(T const &a, T const &b)
 {
 	return (b <= a ? b : a);
 }
 
 template<typename T>
-T		max(T a, T b)
+T const &	max(T const &a, T const &b)
 {
 	return (b >= a ? b : a);
 }
